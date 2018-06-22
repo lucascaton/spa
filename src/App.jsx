8f1a89750fc505053b6726f8339741ddb4a04ef4
Lucas Caton <lucascaton@gmail.com>
2018-06-22 12:43:05 +1000

    Add Google Material Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
+import Button from '@material/react-button/dist';
+import TopAppBar from '@material/react-top-app-bar/dist';
 
 import Home from './Home';
 import About from './About';
 
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
+          <TopAppBar short title="React SPA with Material Design" />
 
-          <hr />
+          <nav>
+            <Link to="/">
+              <Button raised>Home</Button>
+            </Link>{' '}
+            <Link to="/about">
+              <Button raised>About</Button>
+            </Link>
+          </nav>
 
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
         </div>
       </Router>
     );
   }
 }
-
-export default App;
